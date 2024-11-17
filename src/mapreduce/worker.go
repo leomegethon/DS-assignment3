2e1b45e122472b2cf15360d51fb6b051fcb68060
leomegethon <youngukpark@kma.ac.kr>
2024-11-17 23:31:21 +0900

    Update worker.go

--- src/mapreduce/worker.go
+++ src/mapreduce/worker.go
@@ -1,92 +1,66 @@
 package mapreduce
 
-import "fmt"
-import "os"
-import "log"
-import "net/rpc"
-import "net"
-import "container/list"
+import (
+"container/list"
+"fmt"
+)
+type WorkerInfo struct {
+address string
+}
+func AssignJobsToWorkers(mr *MapReduce, doneChannel chan int, job JobType, nJobs int, nJobsOther int) {
+for i := 0; i < nJobs; i++ {
+go func(jobNum int) {
+for {
+worker := <-mr.registerChannel
+args := &DoJobArgs{mr.file, job, jobNum, nJobsOther}
+var reply DoJobReply
+ok := call(worker, "Worker.DoJob", args, &reply)
+if ok {
 
-// Worker is a server waiting for DoJob or Shutdown RPCs
-
-type Worker struct {
-	name   string
-	Reduce func(string, *list.List) string
-	Map    func(string) *list.List
-	nRPC   int
-	nJobs  int
-	l      net.Listener
+doneChannel <- 1
+mr.registerChannel <- worker
+break
+} else {
+fmt.Printf("Worker %s failed on job %d. Retrying...\n", worker, jobNum)
+}
+}
+}(i)
+}
 }
 
-// The master sent us a job
-func (wk *Worker) DoJob(arg *DoJobArgs, res *DoJobReply) error {
-	fmt.Printf("Dojob %s job %d file %s operation %v N %d\n",
-		wk.name, arg.JobNumber, arg.File, arg.Operation,
-		arg.NumOtherPhase)
-	switch arg.Operation {
-	case Map:
-		DoMap(arg.JobNumber, arg.File, arg.NumOtherPhase, wk.Map)
-	case Reduce:
-		DoReduce(arg.JobNumber, arg.File, arg.NumOtherPhase, wk.Reduce)
-	}
-	res.OK = true
-	return nil
+func (mr *MapReduce) KillWorkers() *list.List {
+l := list.New()
+for _, w := range mr.Workers {
+DPrintf("DoWork: shutdown %s\n", w.address)
+args := &ShutdownArgs{}
+var reply ShutdownReply
+ok := call(w.address, "Worker.Shutdown", args, &reply)
+if !ok {
+fmt.Printf("DoWork: RPC %s shutdown error\n", w.address)
+} else {
+l.PushBack(reply.Njobs)
+}
+}
+return l
 }
 
-// The master is telling us to shutdown. Report the number of Jobs we
-// have processed.
-func (wk *Worker) Shutdown(args *ShutdownArgs, res *ShutdownReply) error {
-	DPrintf("Shutdown %s\n", wk.name)
-	res.Njobs = wk.nJobs
-	res.OK = true
-	wk.nRPC = 1 // OK, because the same thread reads nRPC
-	wk.nJobs--  // Don't count the shutdown RPC
-	return nil
+func (mr *MapReduce) RunMaster() *list.List {
+
+mapDoneChannel := make(chan int, mr.nMap)
+reduceDoneChannel := make(chan int, mr.nReduce)
+
+
+AssignJobsToWorkers(mr, mapDoneChannel, Map, mr.nMap, mr.nReduce)
+
+for i := 0; i < mr.nMap; i++ {
+<-mapDoneChannel
 }
 
-// Tell the master we exist and ready to work
-func Register(master string, me string) {
-	args := &RegisterArgs{}
-	args.Worker = me
-	var reply RegisterReply
-	ok := call(master, "MapReduce.Register", args, &reply)
-	if ok == false {
-		fmt.Printf("Register: RPC %s register error\n", master)
-	}
+AssignJobsToWorkers(mr, reduceDoneChannel, Reduce, mr.nReduce, mr.nMap)
+
+for i := 0; i < mr.nReduce; i++ {
+<-reduceDoneChannel
 }
 
-// Set up a connection with the master, register with the master,
-// and wait for jobs from the master
-func RunWorker(MasterAddress string, me string,
-	MapFunc func(string) *list.List,
-	ReduceFunc func(string, *list.List) string, nRPC int) {
-	DPrintf("RunWorker %s\n", me)
-	wk := new(Worker)
-	wk.name = me
-	wk.Map = MapFunc
-	wk.Reduce = ReduceFunc
-	wk.nRPC = nRPC
-	rpcs := rpc.NewServer()
-	rpcs.Register(wk)
-	os.Remove(me) // only needed for "unix"
-	l, e := net.Listen("unix", me)
-	if e != nil {
-		log.Fatal("RunWorker: worker ", me, " error: ", e)
-	}
-	wk.l = l
-	Register(MasterAddress, me)
-
-	// DON'T MODIFY CODE BELOW
-	for wk.nRPC != 0 {
-		conn, err := wk.l.Accept()
-		if err == nil {
-			wk.nRPC -= 1
-			go rpcs.ServeConn(conn)
-			wk.nJobs += 1
-		} else {
-			break
-		}
-	}
-	wk.l.Close()
-	DPrintf("RunWorker %s exit\n", me)
+return mr.KillWorkers()
 }
